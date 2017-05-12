4f2161d608e8b749e0093a2de39d563773d901a4
kevin jia <kevinjia1984@gmail.com>
2017-05-12 16:23:44 +1000

    Re-Submit based on the review
    Add some comments and make the code ok with pep8 checking

--- media.py
+++ media.py
@@ -5,6 +5,7 @@ class Movie() :
     VALID_RATING = ["G","PG","PG-13","R"]
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
+    	# this constructor will initial the following instance variables
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
